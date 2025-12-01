c64bad2961f9594e63e8b93963dbf75d0289ac45
Shibili8 <shibilishibizz99@gmail.com>
2025-12-01 11:59:34 +0530

    first commit

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use(
     credentials: true,
   })
 );
+
 app.options("*", cors());
 
 
--- utils/generateToken.js
+++ utils/generateToken.js
@@ -5,13 +5,14 @@ const generateToken = (res, userId) => {
     expiresIn: "7d",
   });
 
+  const isProduction = process.env.NODE_ENV === "production";
+
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    secure: isProduction,                     // true only on Render HTTPS
+    sameSite: isProduction ? "none" : "lax",  // none only on https
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
-  
 };
 
 export default generateToken;
