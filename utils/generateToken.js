896d3f69f758d0ad6cbe56f8201a8e34ddf501e5
Shibili8 <shibilishibizz99@gmail.com>
2025-12-01 14:26:40 +0530

    first commit

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,16 +1,13 @@
-import jwt from "jsonwebtoken";
-
 const generateToken = (res, userId) => {
   const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
 
+  // Always allow cross-origin cookie for Render + localhost
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: true,          // REQUIRED for https (Render)
-    sameSite: "none",      // REQUIRED for cross-site cookies
+    secure: true,           
+    sameSite: "none",       
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
-
-export default generateToken;
